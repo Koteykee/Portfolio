b1315934adc0b4ef907f713eb30729e734c0131f
Koteykee <koteykeethecat@gmail.com>
2026-03-18 15:33:25 +0300

    Footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <footer className="w-full bg-[#f3f0ed] border-t border-[#e2deda]">
       <div className="container h-18 flex items-center justify-center">
         <p className="text-[#79542e]">
-          2026 Maria Ridal. Thank you for taking your time to visit my page!
+          © 2026 Maria Ridal. Thank you for taking your time to visit my page!
         </p>
       </div>
     </footer>
